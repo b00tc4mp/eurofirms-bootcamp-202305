a19cbdf35165ae7590d793110a07378630797551
Joan Sala <joan.sala.baucells@gmail.com>
2023-05-31 19:41:07 +0200

    modified web app

--- staff/joan-sala/web/calculadora/main.js
+++ staff/joan-sala/web/calculadora/main.js
@@ -1,6 +1,13 @@
 var a=document.querySelector('input[name=a]');
 var b=document.querySelector('input[name=b]')
 var result=document.querySelector('.result')
+
+/*SUMAR Number 1 */ 
+var addition=document.querySelector('#multiply');
+addition.onclick = function(){
+    result.innerHTML = parseFloat(a.value) + parseFloat(b.value);
+}
+
 /*MULTIPLICAR Number 1 */ 
 var multiply=document.querySelector('#multiply');
 multiply.onclick = function(){
