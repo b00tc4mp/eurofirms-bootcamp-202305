786b82edd5e8e4d1c7b169c45da583dd33747db7
Joan Sala <joan.sala.baucells@gmail.com>
2023-06-03 21:01:15 +0200

    modifications in various .js(main,logic,data) for Manu

--- staff/joan-sala/web/app/main.js
+++ staff/joan-sala/web/app/main.js
@@ -1,75 +1,4 @@
 /**
- * DATA
- */
-
-var users = []
-
-users.push({ name: 'Pin Ocho', email: 'pin@ocho.com', password: '123123123' })
-users.push({ name: 'John Doe', email: 'john@doe.com', password: '123123123' })
-users.push({ name: 'Ada Love', email: 'ada@love.com', password: '123123123' })
-
-/**
- * LOGIC
- */
-
-function registerUser(name, email, password) {
-    var user
-
-    for (var i = 0; i < users.length; i++) {
-        var _user = users[i]
-
-        if (_user.email === email) {
-            user = _user
-
-            break
-        }
-    }
-
-    if (user !== undefined)
-        return false
-    else {
-        user = {}
-
-        user.name = name
-        user.email = email
-        user.password = password
-
-        users.push(user)
-
-        return true
-    }
-}
-
-function authenticateUser(email, password) {
-    var user
-
-    for (var i = 0; i < users.length; i++) {
-        var _user = users[i]
-
-        if (_user.email === email) {
-            user = _user
-
-            break
-        }
-    }
-
-    if (user === undefined)
-        return false
-    else if (user.password !== password)
-        return false
-    else
-        return true
-}
-funnction retriewUser(email){
-    for(var i=0; i <user.length;i++){
-        if(email===user[i].email){
-            return false
-        }
-    }
-
-}
-
-/**
  * PRESENTATION
  */
 
@@ -81,13 +10,13 @@ var registerForm = registerView.querySelector('.register-form')
 registerForm.onsubmit = function (event) {
     event.preventDefault()
 
-    var nameInput = registerForm.querySelector('#name')
+    var nameInput = registerForm.querySelector('#register-name')
     var name = nameInput.value
 
-    var emailInput = registerForm.querySelector('#email')
+    var emailInput = registerForm.querySelector('#register-email')
     var email = emailInput.value
 
-    var passwordInput = registerForm.querySelector('#password')
+    var passwordInput = registerForm.querySelector('#register-password')
     var password = passwordInput.value
 
     var result = registerUser(name, email, password)
@@ -95,9 +24,20 @@ registerForm.onsubmit = function (event) {
     if (result === false)
         alert('User already exists')
     else {
+        registerForm.reset()
+
         registerView.classList.add('off')
         loginView.classList.remove('off')
     }
+}
+
+var registerLoginLink = registerView.querySelector('.register-login-link')
+
+registerLoginLink.onclick = function (event) {
+    event.preventDefault()
+
+    registerView.classList.add('off')
+    loginView.classList.remove('off')
 }
 
 // login
@@ -108,26 +48,87 @@ var loginForm = loginView.querySelector('.login-form')
 loginForm.onsubmit = function (event) {
     event.preventDefault()
 
-    var emailInput = loginForm.querySelector('#email')
+    var emailInput = loginForm.querySelector('#login-email')
     var email = emailInput.value
 
-    var passwordInput = loginForm.querySelector('#password')
+    var passwordInput = loginForm.querySelector('#login-password')
     var password = passwordInput.value
 
     var result = authenticateUser(email, password)
 
-    if (result === false) //(!result)
+    if (result === false)
         alert('Wrong credentials')
     else {
-        loginView.classList.add('off')
-        homeView.classList.remove('off')
-        
-        var user=retriewUser(email)
-    
+        loginForm.reset()
+
+        const user = retrieveUser(email)
+
+        if (user === null)
+            alert('User not found')
+        else {
+            loginView.classList.add('off')
+
+            const homeTitle = homeView.querySelector('.home-title')
+            homeTitle.innerText = 'Hello, ' + user.name + '!'
+
+            homeView.classList.remove('off')
+        }
     }
+}
+
+var loginRegisterLink = loginView.querySelector('.login-register-link')
+
+loginRegisterLink.onclick = function (event) {
+    event.preventDefault()
+
+    loginView.classList.add('off')
+    registerView.classList.remove('off')
 }
 
 // home
 
 var homeView = document.querySelector('.home-view')
+var homeCreatePostModal = homeView.querySelector('.home-create-post-modal')
+var homeLogoutButton = homeView.querySelector('.home-logout-button')
+var homeCreatePostForm = homeView.querySelector('.home-create-post-form')
 
+homeLogoutButton.onclick = function () {
+    homeView.classList.add('off')
+    loginView.classList.remove('off')
+}
+
+var homeCreatePostButton = homeView.querySelector('.home-create-post-button')
+
+homeCreatePostButton.onclick = function () {
+    homeCreatePostModal.classList.remove('off')
+}
+
+var homeCreatePostCancelButton = homeCreatePostForm.querySelector('.home-create-post-cancel-button')
+
+homeCreatePostCancelButton.onclick = function (event) {
+    event.preventDefault()
+
+    homeCreatePostForm.reset()
+
+    homeCreatePostModal.classList.add('off')
+}
+
+homeCreatePostForm.onsubmit = function (event) {
+    event.preventDefault()
+
+    var imageInput = homeCreatePostForm.querySelector('#create-post-url')
+    var image = imageInput.value
+
+    var textInput = homeCreatePostForm.querySelector('#create-post-text')
+    var text = textInput.value
+
+    var result = createPost(image, text)
+
+    if (result === false)
+        alert('Cannot create post')
+    else {
+        homeCreatePostForm.reset()
+
+        homeCreatePostModal.classList.add('off')
+    }
+}
