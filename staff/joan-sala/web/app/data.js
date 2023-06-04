a6db0e71e0dbfef227ac1d20d0491a9af7970fe6
Joan Sala <joan.sala.baucells@gmail.com>
2023-06-04 21:05:44 +0200

    pruebas

--- staff/joan-sala/web/app/data.js
+++ staff/joan-sala/web/app/data.js
@@ -11,3 +11,5 @@ users.push({ name: 'Ada Love', email: 'ada@love.com', password: '123123123' })
 var posts = []
 
 posts.push({id: 1, image: 'https://cdn.mos.cms.futurecdn.net/HsDtpFEHbDpae6wBuW5wQo.jpg', text: 'black hole'})
+
+posts.push({id: 2, image: 'https://images.freeimages.com/images/previews/6bd/laundry-1522596.jpg', text: 'clothing label'})
