b97aefcaabaac2a31582f4834336a6b4cef1993d
Mario Aller <jmarioav@me.com>
2023-06-05 19:57:10 +0200

    update 5 nav posts refactoring

--- staff/mario-aller/web/app/data.js
+++ staff/mario-aller/web/app/data.js
@@ -22,5 +22,6 @@ users.push({ name: 'Peregrin Tuk', email: 'pippin@comarca.com', password: 'gondo
 var posts = []
 
 // Carga inicial de Posts
-posts.push({ id: 0, image: 'https://cdn.eso.org/images/thumb700x/eso1103a.jpg', text: 'Orion' })
-posts.push({ id: 1, image: 'https://img2.rtve.es/i/?w=1600&i=1522754812071.jpg', text: 'Via Lactea' })
+posts.push({ id: 0, text: 'Orion', image: 'https://cdn.eso.org/images/thumb700x/eso1103a.jpg'})
+posts.push({ id: 1, text: 'Via Lactea', image: 'https://img2.rtve.es/i/?w=1600&i=1522754812071.jpg'})
+posts.push({ id: 1, text: 'Luna', image: 'https://images.ecestaticos.com/PPi0QU13Mwf7rvIVo6xdGUuSWQU=/49x185:2271x1363/557x418/filters:fill(white):format(jpg)/f.elconfidencial.com%2Foriginal%2F2dd%2F315%2Fa26%2F2dd315a269978872b1037d7b781c34b5.jpg'})
