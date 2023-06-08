2c2c1d79c38f380d74ddc854e861b2fb03de1765
Bermy Rosillo <bermyrosillo@gmail.com>
2023-06-08 10:47:39 +0200

    update app

---
+++ staff/bermy-rosillo/web/app copy/data/users.js
@@ -0,0 +1,4 @@
+var users = []
+users.count=0
+users.push({id:++users.count, name: 'Pin Ocho', email: 'pin@ocho.com', password: '1234' })
+users.push({id:++users.count, name:'gato felix', email: 'gato@felix.com', password:'1234'})
