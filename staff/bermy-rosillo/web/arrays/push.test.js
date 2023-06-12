6aa9666da9c057e65cc0668cdc2b558a16f8bbb2
Bermy Rosillo <bermyrosillo@gmail.com>
2023-06-12 18:18:01 +0200

    arrays methods

---
+++ staff/bermy-rosillo/web/arrays/push.test.js
@@ -0,0 +1,4 @@
+const animals = ['pigs','dogs','cats']
+const count = push( animals,'cows')
+console.log(count)
+console.log(animals)
---
+++ staff/bermy-rosillo/web/arrays/slice.test.js
@@ -0,0 +1,25 @@
+const array =[10,20,30,40,50,60]
+
+const result = slice(array, 2)
+console.log(result) // [30,40, 50, 60]
+
+const result2 = slice(array, 2, 4)
+console.log(result2) // [30,40]
+
+const result3 = slice(array, 0, 10)
+console.log(result3) // [10, 20, 30, 40, 50, 60]
+
+const result4 = slice(array, 5, 3)
+console.log(result4) // []
+
+const result5 = slice(array)
+console.log(result5) // [10,20,30,40,50,60]
+
+const result6 = slice(array, -2)
+console.log(result6) // [50, 60]
+
+const result7 = slice(array, -4, -1)
+console.log(result7) // [30, 40, 50]
+
+const result8 = slice(array, -20, -1)
+console.log(result8) // TODO
