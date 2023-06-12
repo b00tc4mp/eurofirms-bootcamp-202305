5dd944fab8584c04fd826c5ed9ed6952501d87a3
Manuel Barzi <manuelbarzi@gmail.com>
2023-06-12 13:08:56 +0200

    add arrow function demo

---
+++ stuff/js/arrow-functions.js
@@ -0,0 +1,12 @@
+//function hello(to) {
+//    console.log('hello ' + to)
+//}
+
+//const hello = function(to) {
+//    console.log('hello ' + to)
+//}
+
+const hello = to => console.log('hello ' + to)
+
+hello('Pepito')
+// VM739: 9 hello Pepito
