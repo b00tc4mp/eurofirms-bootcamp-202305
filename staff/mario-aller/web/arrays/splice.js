f28ae56d91f5cff7b93961cb85b006729f4e9fe1
Mario Aller <jmarioav@me.com>
2023-06-13 19:10:32 +0200

    array splice

---
+++ staff/mario-aller/web/arrays/splice.js
@@ -0,0 +1,30 @@
+function splice(array, startIndex, deleteCount, ...items) {
+    if (startIndex < 0) startIndex += array.length
+    if (!deleteCount) deleteCount = 1
+    const modif = []
+    const segment = []
+
+    // Parte invariante izq del array
+    for (let i = 0; i < startIndex; i++)
+        modif[modif.length] = array[i]
+
+    // Parte interior
+    for (let i = 0; i < items.length; i++)
+        modif[modif.length] = items[i]
+
+    // Parte invariante der del array
+    for (let i = startIndex + deleteCount; i < array.length; i++)
+        modif[modif.length] = array[i]
+
+    // Segmento
+    for (let i = startIndex; i < startIndex + deleteCount; i++)
+        segment[segment.length] = array[i]
+
+    // Modificacion del array inicial
+    array.length = modif.length
+    for (let i = 0; i < modif.length; i++)
+        array[i] = modif[i]
+
+    // Retornos
+    return segment
+}
---
+++ staff/mario-aller/web/arrays/splice.test.js
@@ -0,0 +1,36 @@
+describe("TEST splice");
+
+describe("CASE with numbers");
+
+console.log('[10, 20, 30, 40, 50, 60, 70, 80, 90]')
+const numbers = [10, 20, 30, 40, 50, 60, 70, 80, 90];
+const result = splice(numbers, 1, 2);
+
+console.log('splice(numbers, 1, 2)')
+console.log(result);
+console.log(numbers);
+
+const numbers2 = [10, 20, 30, 40, 50, 60, 70, 80, 90];
+const result2 = splice(numbers2, 3, 0, 100, 200);
+console.log('splice(numbers2, 3, 0, 100, 200)')
+console.log(result2);
+console.log(numbers2);
+
+const numbers3 = [10, 20, 30, 40, 50, 60, 70, 80, 90];
+const result3 = splice(numbers3, 3, 2, 100, 200);
+console.log('splice(numbers3, 3, 2, 100, 200)')
+console.log(result3);
+console.log(numbers3);
+
+const numbers4 = [10, 20, 30, 40, 50, 60, 70, 80, 90];
+const result4 = splice(numbers4, 3, 2, 100, 200, 300, 400);
+console.log('splice(numbers4, 3, 2, 100, 200, 300, 400)')
+console.log(result4);
+console.log(numbers4);
+
+const numbers5 = [10, 20, 30, 40, 50, 60, 70, 80, 90];
+const result5 = splice(numbers5, -3, 0, 100, 200);
+console.log('splice(numbers5, -3, 0, 100, 200)')
+console.log(result5);
+console.log(numbers5);
+
