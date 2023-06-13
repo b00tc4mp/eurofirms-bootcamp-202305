e93b9c7ffb0d3d62ca65e995fb1d37d992f939a7
Mario Aller <jmarioav@me.com>
2023-06-13 14:21:00 +0200

    array concat

---
+++ staff/mario-aller/web/arrays/concat.test.js
@@ -0,0 +1,63 @@
+const people = [
+    {
+        name: 'Pepito Grillo',
+        age: 30,
+        origin: 'Wonderland',
+        colors: ['red', 'blue', 'yellow']
+    },
+    {
+        name: 'Wendy Darling',
+        age: 16,
+        origin: 'Wonderland',
+        colors: ['pink', 'skyblue']
+    },
+    {
+        name: 'Peter Pan',
+        age: 18,
+        origin: 'Wonderland',
+        colors: ['green', 'blue']
+    },
+    {
+        name: 'James Hook',
+        age: 40,
+        origin: 'Wonderland',
+        colors: ['red', 'black']
+    }
+]
+
+const barbies = [
+    {
+        name: 'Beachy',
+        hair: 'blonde',
+        eyes: 'blue',
+        dress: 'bikini'
+    },
+    {
+        name: 'Mermaid',
+        hair: 'red',
+        eyes: 'green',
+        dress: 'scales'
+    },
+    {
+        name: 'Veterinary',
+        hair: 'blonde',
+        eyes: 'lila',
+        dress: 'dress'
+    },
+    {
+        name: 'Disco',
+        hair: 'brown',
+        eyes: 'gray',
+        dress: 'jumpsuit'
+    },
+    {
+        name: 'Margot',
+        hair: 'blonde',
+        eyes: 'blue',
+        dress: 'dress'
+    }
+]
+
+
+console.log (concat(people,barbies,people))
+
