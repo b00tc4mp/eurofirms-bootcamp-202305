c898841a49505eccad47aeafab9964574429de33
PAULADEJUANSEGURA <pauladejuansegura34@protonmail.com>
2023-06-14 09:41:32 +0200

    arrays

---
+++ staff/paula-de-juan/web/arrays/splice.1.js
@@ -0,0 +1,7 @@
+function splice(array, startIndex, deleteCount, ...items){
+    const result = []
+
+for (let i = 0; i < startIndex; i++){
+   
+
+}
---
+++ staff/paula-de-juan/web/arrays/splice.test.js
@@ -0,0 +1,17 @@
+describe("TEST splice");
+
+describe("CASE with months");
+
+const months = ['Jan', 'Feb', 'March', 'April', 'June'];
+
+const result = splice(months, 3, 0, 'Feb', 'May')
+
+console.log(months)
+
+
+console.log(result)
+/*
+const months2 = ['Jan', 'March', 'April', 'June'];
+const result2 = splice(months, 3, 0, 'Feb', 'May')
+console.log(result2)
+console.log(months2)*/
