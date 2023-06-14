26be37805f9540891cf8598e5a4782efe96bf3b9
Joan Sala <joan.sala.baucells@gmail.com>
2023-06-15 00:22:08 +0200

    (splice|splice.js) (sort|sortt.js)

--- staff/joan-sala/web/arrays/sort.test.js
+++ staff/joan-sala/web/arrays/sort.test.js
@@ -1,5 +1,6 @@
-describe ('TEST sor')
+   describe ('TEST sor')
 
+describe(' CASE sort by first numbers (of each number)')
 const array = [20, 67, 40, 320, 10, 405, 70]
 
 const result = sort(array)
@@ -7,6 +8,7 @@ const result = sort(array)
 console.log(array)
 console.log(result)
 
+describe(' CASE sort by letter')
 const array2 = ['March', 'Jan', 'Feb', 'Dec']
 
 const result2 = sort(array2)
@@ -14,7 +16,7 @@ const result2 = sort(array2)
 console.log(array2)
 console.log(result2)
 
-
+describe(' CASE order and return inverter')
 const array3 = [10, 2, 30, 5, 4, 90, 54, 9]
 
 const numbersOrder = (a,b) => {return a-b}
