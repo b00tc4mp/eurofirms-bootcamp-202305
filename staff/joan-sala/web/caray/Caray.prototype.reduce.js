a29daf1cd8d085a75b5af962afe30a8cb32a640e
Joan Sala <joan.sala.baucells@gmail.com>
2023-06-18 19:49:31 +0200

    reduce

---
+++ staff/joan-sala/web/caray/Caray.prototype.reduce.js
@@ -0,0 +1,10 @@
+Caray.prototype.reduce = function(callback, initialValue){
+    let index =   0
+
+    if(initialValue === undefined){
+        initialValue = this[0]
+
+        index = 0
+    }
+    let accumulator = initialValue
+}
