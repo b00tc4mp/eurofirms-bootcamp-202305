1e9bda757d7c5adc4975103c28ef4dc7f4e8d42a
Joan Sala <joan.sala.baucells@gmail.com>
2023-06-20 00:04:14 +0200

    App jsx

--- staff/joan-sala/web/app/data/models/users.js
+++ staff/joan-sala/web/app/data/models/users.js
@@ -1,8 +1,7 @@
-function User(name, email, password) {
-    this.id = ++User.count
-    this.name = name
-    this.email = email
-    this.password = password
-}
+const users = []
 
-User.count = -1
+// populate users
+
+users.push(new User('Pin Ocho', 'pin@ocho.com', '123123123'))
+users.push(new User('John Doe', 'john@doe.com', '123123123'))
+users.push(new User('Ada Love', 'ada@love.com', '123123123'))
--- staff/joan-sala/web/caray/Caray.js
+++ staff/joan-sala/web/caray/Caray.js
@@ -1,14 +1,35 @@
 function Caray(...items) {
-    for (let i = 0; i < items.length; i++) {
-        this[i] = items[i]
-    }
-
-    this.length = items.length
-}
-Caray.prototype.every = function (callback) {
-    for (let i = 0; i < this.length; i++) {
-      if (!callback(this[i])) 
-        return false
-    }
-    return true
+  for (let i = 0; i < items.length; i++) {
+    this[i] = items[i]
   }
+
+  this.length = items.length
+}
+
+Caray.prototype.every = function (callback) {
+  for (let i = 0; i < this.length; i++) {
+    if (!callback(this[i]))
+      return false
+  }
+  return true
+}
+
+Caray.prototype.forEach = function (callback) {
+  for (let i = 0; i < this.length; i++) {
+      const element = this[i]
+
+      callback(element)
+  }
+}
+
+Caray.prototype.find = function (callback) {
+  for (let i = 0; i < this.length; i++) {
+      const element = this[i]
+
+      const result = callback(element)
+
+      if (result)
+          return element
+  }
+}
+
