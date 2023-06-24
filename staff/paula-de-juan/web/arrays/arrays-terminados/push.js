403672371dd7d2aec214965dd06f84cb3c99fc20
PAULADEJUANSEGURA <pauladejuansegura34@protonmail.com>
2023-06-24 18:38:35 +0200

    PUSH returns array.length

--- staff/paula-de-juan/web/arrays/arrays-terminados/push.js
+++ staff/paula-de-juan/web/arrays/arrays-terminados/push.js
@@ -1,19 +1,5 @@
 function push(array, element) {
-    if (arguments.length === 2) {
-        const index = array.length
-
-        array[index] = element
-
-        return array.length
-    } else if (arguments.length > 2) {
-        for (let i = 1; i < arguments.length; i++) {
-            const element = arguments[i]
-
-            const index = array.length
-
-            array[index] = element
-        }
-
-        return array.length
-    }
+    const index = array.length
+    array[index] = element
+    return array.length
 }
