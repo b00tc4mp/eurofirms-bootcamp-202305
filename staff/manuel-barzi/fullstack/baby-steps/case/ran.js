6f039d3fcf1de813d528db1f328c9d106173a5cd
Manuel Barzi <manuelbarzi@gmail.com>
2023-06-26 13:12:48 +0200

    add baby steps in node

---
+++ staff/manuel-barzi/fullstack/baby-steps/case/ran.js
@@ -0,0 +1,32 @@
+// $ node ran hola mundo
+// hoLA MUnDo
+
+// $ node ran hola mundo
+// HoLa munDO
+
+// $ node ran hola mundo
+// hOLa MUNdo
+
+let text = ''
+
+for (let i = 2; i < process.argv.length; i++) {
+    const item = process.argv[i]
+
+    text += item
+
+    if (i < process.argv.length - 1) text += ' '
+}
+
+let random = ''
+
+for (let i = 0; i < text.length; i++) {
+    const char = text[i]
+
+    if (Math.random() < 0.5)
+        random += char.toLowerCase()
+    else
+        random += char.toUpperCase()
+}
+
+console.log(random)
+
