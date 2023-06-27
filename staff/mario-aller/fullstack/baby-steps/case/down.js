03ab545084dee6f41693b34eeb2e3fcfeaef3792
Mario Aller <jmarioav@me.com>
2023-06-27 10:25:54 +0200

    update down

--- staff/mario-aller/fullstack/baby-steps/case/down.js
+++ staff/mario-aller/fullstack/baby-steps/case/down.js
@@ -1,3 +1,6 @@
-const text = process.argv[2]
-
-console.log(text.toLowerCase())
+if (process.argv.length < 3) console.log('Error: argumentos incorrectos')
+else {
+    let text = process.argv[2]
+    for (let i=3;i< process.argv.length;i++) text += ' ' + process.argv[i]
+    console.log(text.toLowerCase())
+}
