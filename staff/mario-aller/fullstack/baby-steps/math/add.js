947a5239bbf4c1259aa73dd22218d7c562c208b2
Mario Aller <jmarioav@me.com>
2023-06-27 10:46:41 +0200

    create add file

---
+++ staff/mario-aller/fullstack/baby-steps/math/add.js
@@ -0,0 +1,7 @@
+if (process.argv.length < 4) console.log('Error: argumentos incorrectos')
+else {
+    let sum = 0
+    for (let i = 2; i < process.argv.length; i++)
+        sum += Number(process.argv[i])
+    console.log(sum)
+}
