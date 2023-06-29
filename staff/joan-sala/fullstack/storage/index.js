e5e36c83115572a6e45393214e8b75d79f85bcc3
Joan Sala <joan.sala.baucells@gmail.com>
2023-06-29 08:01:30 +0200

    single quotes

--- staff/joan-sala/fullstack/storage/index.js
+++ staff/joan-sala/fullstack/storage/index.js
@@ -14,14 +14,14 @@
  * $ node . remove 1
  */
 // @example READ STORAGEcd 
-const fs = require("fs")
+const fs = require('fs')
 
 const operation = process.argv[2]
 
-const file = "./storage.json"
+const file = './storage.json'
 
 if (!operation) {            //callback
-  fs.readFile(file, "utf8", (error, json) => {
+  fs.readFile(file, 'utf8', (error, json) => {
     if (error) {
       console.error(error.message)
 
@@ -34,7 +34,7 @@ if (!operation) {            //callback
     console.table(data) //mostrar
   })
               //AGREGAR ARCHIVO
-} else if (operation === "add") {
+} else if (operation === 'add') {
   const what = process.argv[3]
   const where = process.argv[4]
   const when = new Date().toISOString()  //transforma a fecha xxxx-xx-xxTxx:xx:xx_xxZ
@@ -43,17 +43,17 @@ if (!operation) {            //callback
   const item = { what, where, when }
 
 
-  fs.readFile(file, "utf8", (error, json) => {
+  fs.readFile(file, 'utf8', (error, json) => {
     if (error) {
       console.error(error.message)
 
       return
     }
-
+                     //string a objeto
     const data = JSON.parse(json)
     data.push(item)
 
-    const dataString = JSON.stringify(data, null, " ")
+    const dataString = JSON.stringify(data, null, ' ')
 
     fs.writeFile(file, dataString, (error) => {
       if (error) {
@@ -64,9 +64,9 @@ if (!operation) {            //callback
     })
   })   
     // @example UPDATE ITEM
-} else if (operation === "update") {
+} else if (operation === 'update') {
     // HINT update object const item = items[index], item.what = ...
-  fs.readFile(file, "utf8", (error, json) => {
+  fs.readFile(file, 'utf8', (error, json) => {
     if (error) {
       console.error(error.message)
 
@@ -76,21 +76,21 @@ if (!operation) {            //callback
     const data = JSON.parse(json);
 
     const index = parseInt(process.argv[3]);
-    const what = process.argv[4] && process.argv[4] !== "," ? process.argv[4] : data[index].what
-    const where = process.argv[5] && process.argv[5] !== "," ? process.argv[5] : data[index].where;
+    const what = process.argv[4] && process.argv[4] !== ',' ? process.argv[4] : data[index].what
+    const where = process.argv[5] && process.argv[5] !== ',' ? process.argv[5] : data[index].where;
     const when = new Date().toISOString()
 
     const item = { what, where, when }
 
     if (index >= data.length || isNaN(index)) {
-      console.error("te jodes, no encontrado")
+      console.error('te jodes, no encontrado')
 
       return
     }
 
     data[index] = item
 
-    const dataString = JSON.stringify(data, null, " ")
+    const dataString = JSON.stringify(data, null, ' ')
 
     fs.writeFile(file, dataString, (error) => {
       if (error) {
@@ -102,11 +102,11 @@ if (!operation) {            //callback
   })
 
   // @example REMOVE ITEM
-} else if (operation === "delete") {
+} else if (operation === 'delete') {
     // HINT use arrays .splice
   const index = process.argv[3]
 
-  fs.readFile(file, "utf8", (error, json) => {
+  fs.readFile(file, 'utf8', (error, json) => {
     if (error) {
       console.error(error.message)
 
@@ -117,7 +117,7 @@ if (!operation) {            //callback
 
     data.splice(index, 1);
 
-    const dataString = JSON.stringify(data, null, " ")
+    const dataString = JSON.stringify(data, null, ' ')
 
     //modificar archivo
     fs.writeFile(file, dataString, (error) => {
