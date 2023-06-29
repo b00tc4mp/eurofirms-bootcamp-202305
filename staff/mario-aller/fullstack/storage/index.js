19d294042bc84adab92a3396868018454ae9606f
Mario Aller <jmarioav@me.com>
2023-06-29 13:46:28 +0200

    draft storage OOP

--- staff/mario-aller/fullstack/storage/index.js
+++ staff/mario-aller/fullstack/storage/index.js
@@ -1,51 +1,100 @@
 const fs = require('fs')
 
-function Storage(name) {
-    this.name = name
-    this.items = []
-}
-Storage.prototype.draw = function (text) {
-    console.log('Storage: ' + this.name + text ? ' > ' + text : ' -')
-    console.table(this.items)
-}
-Storage.prototype.toHD = function (file) {
-    const fileName = file ? file : './' + this.name + '.json'
-    fs.writeFile(fileName, JSON.stringify(this.items, null, " "), error => {
-        if (error) console.error('No se puede escribir en HD')
-        this.draw('to HD')
-    })
-}
-Storage.prototype.fromHD = function (file) {
-    const fileName = file ? file : './' + this.name + '.json'
-    fs.readFile(fileName, 'utf8', (error, dataFile) => {
-        this.items = error ? [] : JSON.parse(dataFile)
-        this.draw('from HD')
-    })
-}
-Storage.prototype.itemPut = function (what, where) {
-    fs.readFile('./' + this.name + '.json', 'utf8', function (error, dataFile) {
-        this.items = error ? [] : JSON.parse(dataFile)
-        
-        const when = new Date().toISOString()
-        const item = { what, where, when }
-        this.items.push(item)
+class Storage {
 
-        fs.writeFile('./' + this.name + '.json', JSON.stringify(this.items, null, " "), function(error) {
+    constructor(name) {
+        this.name = name
+        this.items = []
+    }
+
+    draw(text) {
+        console.log('Storage: ' + this.name + text ? ' > ' + text : ' -')
+        console.table(this.items)
+    }
+
+    toHD(file) {
+        const fileName = file ? file : './' + this.name + '.json'
+        fs.writeFile(fileName, JSON.stringify(this.items, null, " "), error => {
             if (error) console.error('No se puede escribir en HD')
-            this.draw('Item input')
-        }.bind(this))
-        
-    }.bind(this))
-}
+            this.draw('to HD')
+        })
+    }
 
+    fromHD(file) {
+        const fileName = file ? file : './' + this.name + '.json'
+        fs.readFile(fileName, 'utf8', (error, dataFile) => {
+            this.items = error ? [] : JSON.parse(dataFile)
+            this.draw('from HD')
+        })
+    }
+
+    itemPut(what, where) {
+        fs.readFile('./' + this.name + '.json', 'utf8', (error, dataFile) => {
+            this.items = error ? [] : JSON.parse(dataFile)
+
+            const when = new Date().toISOString()
+            const item = { what, where, when }
+            this.items.push(item)
+
+            fs.writeFile('./' + this.name + '.json', JSON.stringify(this.items, null, " "), error => {
+                if (error) console.error('No se puede escribir en HD')
+                this.draw('Item input')
+            })
+        })
+    }
+
+    itemUpdate(pos, what, where) {
+        fs.readFile('./' + this.name + '.json', 'utf8', (error, dataFile) => {
+            this.items = error ? [] : JSON.parse(dataFile)
+
+            const what2 = what ? what : this.items[pos].what
+            const where2 = where ? where : this.items[pos].where
+            const when = new Date().toISOString()
+            this.items[pos] = { what: what2, where: where2, when }
+
+            fs.writeFile('./' + this.name + '.json', JSON.stringify(this.items, null, " "), error => {
+                if (error) console.error('No se puede escribir en HD')
+                this.draw('Item input')
+            })
+        })
+    }
+
+    itemDelete(pos) {
+        fs.readFile('./' + this.name + '.json', 'utf8', (error, dataFile) => {
+            this.items = error ? [] : JSON.parse(dataFile)
+
+            this.items.splice(pos, 1)
+
+            fs.writeFile('./' + this.name + '.json', JSON.stringify(this.items, null, " "), error => {
+                if (error) console.error('No se puede escribir en HD')
+                this.draw('Item input')
+            })
+        })
+    }
+}
 
 // main
 
-// populate
 const sto = new Storage('store')
 
-sto.fromHD()
+function errorArg(num, msg) {
+    if (process.argv.length < num) {
+        console.error(msg)
+        return true
+    }
+    return false
+}
 
-sto.itemPut('donuts','home')
-
-sto.draw('main')
+const operation = process.argv[2].toLowerCase()
+switch (operation) {
+    case 'add':
+        if (!errorArg(5, 'add: error arg')) sto.itemPut(process.argv[3], process.argv[4])
+        break
+    case 'udt':
+        if (!errorArg(6, 'update: error arg')) sto.itemPut(process.argv[3], process.argv[4], process.argv[5])
+        break
+    case 'del':
+        if (!errorArg(4, 'delete: error arg')) sto.itemPut(process.argv[3])
+        break
+    default: console.error('Operación incorrecta')
+}
