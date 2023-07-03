d5e8686abfed7f2237ecbef2dfb4d35d5cc0c428
PAULADEJUANSEGURA <pauladejuansegura34@protonmail.com>
2023-07-04 00:09:47 +0200

    a few files of new API and other stuff

---
+++ staff/paula-de-juan/fullstack/api/logic/registerUser.js
@@ -0,0 +1,19 @@
+const context = require('./context')
+
+function registerUser(name, email, password){
+
+    if(typeof name !== 'string') throw new Error('Name is not a string')
+    if(name === '') throw new Error('Name is empty')
+    if(typeof email !== 'string') throw new Error('Emails is not a string')
+    if(email === '') throw new Error('Email is empty')
+    if(typeof password !== 'string') throw new Error('Password is not a string')
+    if(password === '') throw new Error('Password is empty')
+
+    return context.users.findOne({email})
+        .then(user => {
+            if (user) throw new Error('User already exists')
+            
+        return context.users.insertOne({name, email, password})
+        })
+}
+module.exports = registerUser
