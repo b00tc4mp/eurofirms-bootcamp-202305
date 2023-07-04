b767f3f28bb1b302adeb82b4903346fec49f91fd
Mario Aller <jmarioav@me.com>
2023-07-04 22:22:56 +0200

    retrievePosts and createPost finished

--- staff/mario-aller/fullstack/api/logic/createPost.js
+++ staff/mario-aller/fullstack/api/logic/createPost.js
@@ -2,6 +2,13 @@ const ctx = require('./ctx')
 const { ObjectId } = require('mongodb')
 const { stringValid } = require('./helpers/validators')
 
+/**
+ * La función crea una nueva publicación con el ID de autor, el mensaje y la URL de la imagen proporcionados.
+ * @param authorId - El ID del autor que está creando la publicación.
+ * @param msg - El parámetro `msg` es una cadena que representa el contenido de texto de la publicación.
+ * @param img - El parámetro `img` es una cadena que representa la URL de una imagen.
+ * @returns una promesa.
+ */
 function createPost(authorId, msg, img) {
     stringValid(authorId)
     stringValid(msg, ctx.STR_NAME)
