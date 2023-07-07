127982846450046244dcce994fbe47f8ae02544f
Manuel Barzi <manuelbarzi@gmail.com>
2023-07-07 13:13:03 +0200

    add more api route handlers (controllers, middlewares) and calls to logics

--- staff/manuel-barzi/fullstack/api/index.js
+++ staff/manuel-barzi/fullstack/api/index.js
@@ -4,6 +4,10 @@ const mongodb = require('mongodb')
 const context = require('./logic/context')
 const registerUser = require('./logic/registerUser')
 const authenticateUser = require('./logic/authenticateUser')
+const retrieveUser = require('./logic/retrieveUser')
+const createPost = require('./logic/createPost')
+const retrievePosts = require('./logic/retrievePosts')
+const updatePost = require('./logic/updatePost')
 
 const { MongoClient } = mongodb
 
@@ -34,9 +38,9 @@ client.connect()
         })
 
         api.post('/users', jsonBodyParser, (req, res) => {
-            const { name, email, password } = req.body
-
             try {
+                const { name, email, password } = req.body
+
                 registerUser(name, email, password)
                     .then(() => res.status(201).send())
                     .catch(error => res.status(400).json({ error: error.message }))
@@ -46,9 +50,9 @@ client.connect()
         })
 
         api.post('/users/auth', jsonBodyParser, (req, res) => {
-            const { email, password } = req.body
-
             try {
+                const { email, password } = req.body
+
                 authenticateUser(email, password)
                     .then(userId => res.json(userId))
                     .catch(error => res.status(400).json({ error: error.message }))
@@ -56,6 +60,78 @@ client.connect()
                 res.status(400).json({ error: error.message })
             }
         })
+
+        api.get('/users', (req, res) => {
+            try {
+                // inside headers:
+                // headers = {
+                //     authorization: 'Bearer 64a7e52467f817075b908a21',
+                //     'User-Agent': 'insomnia/2023.4.0'
+                //      ...
+                // }
+
+                // const authorization = req.headers.authorization
+                const { authorization } = req.headers
+                const userId = authorization.slice(7)
+
+                retrieveUser(userId)
+                    .then(user => res.json(user))
+                    .catch(error => res.status(400).json({ error: error.message }))
+            } catch (error) {
+                res.status(400).json({ error: error.message })
+            }
+        })
+
+        api.post('/posts', jsonBodyParser, (req, res) => {
+            try {
+                const { authorization } = req.headers
+                const userId = authorization.slice(7)
+
+                const { image, text } = req.body
+
+                createPost(userId, image, text)
+                    .then(() => res.status(201).send())
+                    .catch(error => res.status(400).json({ error: error.message }))
+            } catch (error) {
+                res.status(400).json({ error: error.message })
+            }
+        })
+
+        api.get('/posts', (req, res) => {
+            try {
+                const { authorization } = req.headers
+                const userId = authorization.slice(7)
+
+                retrievePosts(userId)
+                    .then(posts => res.json(posts))
+                    .catch(error => res.status(400).json({ error: error.message }))
+            } catch (error) {
+                res.status(400).json({ error: error.message })
+            }
+        })
+
+        // DONE updatePost (PATCH /posts/:postId)
+
+        api.patch('/posts/:postId', jsonBodyParser, (req, res) => {
+            try {
+                const { authorization } = req.headers
+                const userId = authorization.slice(7)
+
+                //const postId = req.params.postId
+                const { postId } = req.params
+
+                const { image, text } = req.body
+
+                updatePost(userId, postId, image, text)
+                    .then(() => res.status(204).send())
+                    .catch(error => res.status(400).json({ error: error.message }))
+            } catch (error) {
+                res.status(400).json({ error: error.message })
+            }
+        })
+
+        // TODO deletePost (DELETE /posts/:postId)
+        // TODO retrievePost (GET /posts/:postId)
 
         api.listen(9000, () => console.log('API running in port 9000'))
     })
