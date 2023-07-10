c98226fe68861580cf70ba2ab112f2f8a9795aab
Paula Vila <pauvg4@gmail.com>
2023-07-10 21:07:43 +0200

    change branch app and apdate api

---
+++ staff/pau-vila/web/app/logic/registerUser.js
@@ -0,0 +1,22 @@
+function registerUser(name, email, password) {
+    let user
+    const users = db.users
+    for (let i = 0; i < users.length; i++) {
+        const _user = users[i]
+
+        if (_user.email === email) {
+            user = _user
+
+            break
+        }
+    }
+
+    if (user !== undefined)
+        return false
+   
+        user = new User(++db.userIdCount, name, email, password)
+
+        users.push(user)
+        db.users = users
+        return true
+    }
---
+++ staff/pau-vila/web/app/view/pages/Login.jsx
@@ -0,0 +1,45 @@
+function Login(props) {
+    console.log('Login -> render')
+
+    const handleRegisterClick = event => {
+        event.preventDefault()
+
+        props.onRegisterClick()
+    }
+
+    const handleLoginSubmit = event => {
+        event.preventDefault()
+
+        const email = event.target.email.value
+        const password = event.target.password.value
+
+        const result = authenticateUser(email, password)
+
+        if (result === false)
+            alert('wrong credentials')
+        else {
+            context.userId = result
+
+            props.onLoggedIn()
+        }
+    }
+    //target: hace referencia al objeto del elemento DOM
+    //onLoggedIn: redirecciona
+    //onLoggedIn: se refiere a la función q se desencadena despúes de haberse iniciado sesión
+    return <main className="login-view">
+        <h1>Login</h1>
+
+        <form className="login-form" onSubmit={handleLoginSubmit}>
+            <label htmlFor="email">E-mail</label>
+            <input id="email" type="email"></input>
+
+            <label htmlFor="password">Password</label>
+            <input id="password" type="password"></input>
+
+            <button type="submit">Login</button>
+        </form>
+
+        <p>Go to <a className="login-register-link" href="" onClick={handleRegisterClick}>Register</a></p>
+
+    </main>
+}
