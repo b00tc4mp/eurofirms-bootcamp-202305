77fc9feb2c52101bbecd1dead5f7e65bd99370f9
PAULADEJUANSEGURA <pauladejuansegura34@protonmail.com>
2023-07-12 00:49:33 +0200

    changes on API and Application

--- staff/paula-de-juan/fullstack/api/logic/retrieveUser.js
+++ staff/paula-de-juan/fullstack/api/logic/retrieveUser.js
@@ -2,16 +2,17 @@ const context = require('./context')
 const { ObjectId } = require('mongodb')
 const { validateId } = require('./helpers/validators')
 
-function retrieveUser(userId){ validateId(userId)
+function retrieveUser(userId) {
+    validateId(userId)
 
-return context.users.findOne({ _id: new ObjectId(userId) })
-    .then(user =>{
-        if(!user) throw new Error ('user not found')
+    return context.users.findOne({ _id: new ObjectId(userId) })
+        .then(user => {
+            if (!user) throw new Error('user not found')
 
-        delete user._id
-        delete user.password
+            delete user._id
+            delete user.password
 
-        return user
-    })
+            return user
+        })
 }
 module.exports = retrieveUser
