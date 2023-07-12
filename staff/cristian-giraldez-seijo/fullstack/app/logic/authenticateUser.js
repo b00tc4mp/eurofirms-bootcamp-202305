d7062660cddd1f0bab171a848667cd0269e7ffbe
Cristian Giraldez Seijo <crisdemos@gmail.com>
2023-07-12 23:06:18 +0200

    add login user logic with api

--- staff/cristian-giraldez-seijo/fullstack/app/logic/authenticateUser.js
+++ staff/cristian-giraldez-seijo/fullstack/app/logic/authenticateUser.js
@@ -1,21 +1,31 @@
+/**
+ * The function `authenticateUser` sends a POST request to a server to authenticate a user with their
+ * email and password, and returns the user's ID if successful.
+ * @param email - The email parameter is a string that represents the user's email address.
+ * @param password - The password parameter is a string that represents the user's password.
+ * @returns The function `authenticateUser` returns a Promise that resolves to the `userId` if the
+ * authentication is successful. If there is an error, it throws an Error with the error message.
+ */
 function authenticateUser(email, password) {
-    let user
-const users = db.users
+    if (typeof email !== 'string') throw new Error('email is not a string!')
+    if (typeof password !== 'string') throw new Error('password is not a string!')
 
-    for (let i = 0; i < users.length; i++) {
-        const _user = users[i]
-
-        if (_user.email === email) {
-            user = _user
-
-            break
-        }
-    }
-
-    if (user === undefined)
-        return false
-    else if (user.password !== password)
-        return false
-    else
-        return user.id
+    return fetch('http://localhost:9000/users/auth', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ email, password })
+    })
+        .then(res => {
+            if (res.status === 200) return res.json()
+                .then(body => {
+                    const userId = body
+                    return userId
+                })
+            else if (res.status === 400) return res.json().then(body => {
+                const message = body.error
+                throw new Error(message)
+            })
+        })
 }
--- staff/cristian-giraldez-seijo/fullstack/app/view/pages/Login.jsx
+++ staff/cristian-giraldez-seijo/fullstack/app/view/pages/Login.jsx
@@ -1,3 +1,9 @@
+/**
+ * The Login function is a React component that renders a login form and handles user authentication.
+ * @returns The Login component is returning a JSX element, specifically a main element with a
+ * className of "login-view". Inside the main element, there is an h1 element with the text "Login", a
+ * form element with a className of "login-form", and a p element with a link to register.
+ */
 function Login(props) {
     console.log('Login->render')
 
@@ -5,34 +11,34 @@ function Login(props) {
         event.preventDefault()
         props.onRegisterClick()
     }
-    
-const handleLoginSubmit = event => {
-    event.preventDefault()
-    const email = event.target.email.value
-    const password = event.target.password.value
-    const result = authenticateUser(email, password)
 
-    if (result === false) {
-        alert('Wrong credentials!')
-    } else {
-        context.userId = result
-        props.onLoggedIn()
+    const handleLoginSubmit = event => {
+        event.preventDefault()
+        const email = event.target.email.value
+        const password = event.target.password.value
+        try {
+            authenticateUser(email, password)
+                .then(userId => {
+                    context.userId = userId
+                    props.onLoggedIn()
+                })
+                .catch(error => alert(error.message))
+        } catch (error) { alert(error.message) }
     }
-}
     return <main className="login-view">
-    <h1>Login</h1>
-    
-    <form className="login-form" onSubmit={handleLoginSubmit}>
-        <label htmlFor="email">E-mail</label>
-        <input id="email" type="email"></input>
-    
-        <label htmlFor="password">Password</label>
-        <input id="password" type="password"></input>
-    
-        <button type="submit">Login</button>
-    </form>
-    
-<p>Go to <a className="login-register-link" href="" onClick={handleRegisterClick}>Register</a></p>
+        <h1>Login</h1>
+
+        <form className="login-form" onSubmit={handleLoginSubmit}>
+            <label htmlFor="email">E-mail</label>
+            <input id="email" type="email"></input>
+
+            <label htmlFor="password">Password</label>
+            <input id="password" type="password"></input>
+
+            <button type="submit">Login</button>
+        </form>
+
+        <p>Go to <a className="login-register-link" href="" onClick={handleRegisterClick}>Register</a></p>
     </main>
-    
+
 }
