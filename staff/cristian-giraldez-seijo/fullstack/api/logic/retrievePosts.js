7b627ac045c91984366f6a0f96461551a285c2bc
Cristian Giraldez Seijo <crisdemos@gmail.com>
2023-07-23 18:58:58 +0200

    test retrieve posts

--- staff/cristian-giraldez-seijo/fullstack/api/logic/retrievePosts.js
+++ staff/cristian-giraldez-seijo/fullstack/api/logic/retrievePosts.js
@@ -1,38 +1,39 @@
 /**
- * The function retrieves posts from a MongoDB database and transforms the data to include the author's
- * name.
+ * The function retrieves posts from the database and populates the author field, while also sanitizing
+ * the data and adding a "fav" property to indicate if the post is favorited by the user.
  * @param userId - The `userId` parameter is the ID of the user for whom we want to retrieve posts.
  * @returns The function `retrievePosts` is returning a promise that resolves to an array of posts.
  */
-const context = require('./context')
-const { ObjectId } = require('mongodb')
 const { validateId } = require('./helpers/validators')
+const { User, Post } = require('../data')
 
 function retrievePosts(userId) {
     validateId(userId)
 
-    const userObjectId = new ObjectId(userId)
-
-    return context.users.findOne({ _id: userObjectId })
+    return User.findById(userId)
         .then(user => {
             if (!user) throw new Error('user not found')
 
-            return Promise.all([context.posts.find().toArray(), context.users.find().toArray()])
-        })
-        .then(([posts, users]) => {
-            posts.forEach(post => {
-                post.id = post._id.toString()
-                delete post._id
+            //return Post.find({}, '-__v').populate('author', '-email -password -favs -__v').lean()
+            return Post.find({}, '-__v').populate('author', 'name').lean()
+                .then(posts => {
+                    // sanitize
+                    posts.forEach(post => {
+                        post.id = post._id.toString()
+                        delete post._id
 
-                const user = users.find(user => user._id.toString() === post.author.toString())
+                        const { author } = post
 
-                post.author = {
-                    id: user._id.toString(),
-                    name: user.name
-                }
-            })
+                        if (author._id) {
+                            author.id = author._id.toString()
+                            delete author._id
+                        }
 
-            return posts
+                        post.fav = user.favs.some(fav => fav.toString() === post.id)
+                    })
+
+                    return posts
+                })
         })
 }
 
--- staff/cristian-giraldez-seijo/fullstack/api/logic/retrievePosts.test.js
+++ staff/cristian-giraldez-seijo/fullstack/api/logic/retrievePosts.test.js
@@ -1,30 +1,10 @@
-/* The code you provided is connecting to a MongoDB database and retrieving posts using the
-`retrievePosts` function. Here is a breakdown of what the code is doing: */
+require('dotenv').config()
+
 const retrievePosts = require('./retrievePosts')
-const mongodb = require('mongodb')
-const context = require('./context')
+const mongoose = require('mongoose')
 
-const { MongoClient } = mongodb
-
-const client = new MongoClient('mongodb://127.0.0.1:27017')
-
-client.connect()
-    .then(connection => {
-        const db = connection.db('data')
-
-        const users = db.collection('users')
-        const posts = db.collection('posts')
-
-        context.users = users
-        context.posts = posts
-
-        try {
-            return retrievePosts('64a3235e0c5b37d559671976')
-                .then(posts => console.log('posts retrieved', posts))
-                .catch(error => console.error(error))
-        } catch (error) {
-            console.error(error)
-        }
-    })
-    .catch(error => console.error(error))
-    .finally(() => client.close())
+mongoose.connect(`${process.env.MONGODB_URL}/test`)
+.then(() => retrievePosts('64baaad8863d87a1a2c2747b'))
+.then(posts => console.log('posts retrieved', posts))
+.catch(error => console.error(error))
+.finally(() => mongoose.disconnect())
