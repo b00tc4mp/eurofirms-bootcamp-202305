b39eda7148f39ba1fa35b2d21d96e6c8874f24b7
PAULADEJUANSEGURA <pauladejuansegura34@protonmail.com>
2023-07-24 06:09:43 +0200

    updatePost and updatePost.test to mongoose

--- staff/paula-de-juan/fullstack/api/logic/updatePost.js
+++ staff/paula-de-juan/fullstack/api/logic/updatePost.js
@@ -1,7 +1,5 @@
-const context = require('./context')
-const mongodb = require('mongodb')
-const { ObjectId } = mongodb
-
+const { User, Post } = require('../data/models')
+const mongoose = require('mongoose')
 const { validateId, validateUrl, validateText } = require('./helpers/validators')
 
 function updatePost(userId, postId, image, text){
@@ -10,17 +8,20 @@ function updatePost(userId, postId, image, text){
     validateUrl(image)
     validateText(text)
 
-    const userObjectId = new ObjectId(userId)
-    const postObjectId = new ObjectId(postId)
+    //const userObjectId = new ObjectId(userId)
+    //const postObjectId = new ObjectId(postId)
 
-    return Promise.all([context.users.findOne({ _id: userObjectId}), context.posts.findOne({ _id: postObjectId })])
+    return Promise.all([User.findById(userId).lean(), Post.findById(postId)])
         .then(([user, post]) => {
             if(!user) throw new Error('user not found')
             if(!post) throw new Error('post not found')
         
             if(post.author.toString() !== userId) throw new Error('post does not belong to the user')
 
-            return context.posts.updateOne({ _id: postObjectId }, {$set: { image, text } })
+            post.image = image
+            post.text = text
+
+            return post.save()
         })
         .then(() => { })
 }
--- staff/paula-de-juan/fullstack/api/logic/updatePost.test.js
+++ staff/paula-de-juan/fullstack/api/logic/updatePost.test.js
@@ -1,23 +1,10 @@
+require('dotenv').config()
 const updatePost = require('./updatePost')
-const mongodb = require('mongodb')
-const context = require('./context')
+const mongoose = require('mongoose')
 
-const { MongoClient } = mongodb
 
-const client = new MongoClient('mongodb://127.0.0.1:27017')
-
-client.connect()
-    .then(connection => {
-        const db = connection.db('data')
-
-        const users = db.collection('users')
-        const posts = db.collection('posts')
-
-        context.users = users
-        context.posts = posts
-
-        try {
-            return updatePost
-                            
-        }
-    })
+mongoose.connect(`${process.env.MONGODB_URL}/data`)
+    .then(() => updatePost('64b44e086bee5b508dad06c0', '64b44e7a6bee5b508dad06c2', 'https://www.semana.com/resizer/3pvTV-IxzvCcQwyP0FjZzWtOag8=/1280x720/smart/filters:format(jpg):quality(80)/cloudfront-us-east-1.images.arcpublishing.com/semana/DPCC2AM7CRDYHPGAZT53S5C2BQ.jpg', 'Kurt cobain y su amor por los gatos'))
+    .then(() => console.log('post updated'))
+    .catch(error => console.error(error))
+    .finally(() => mongoose.disconnect())
