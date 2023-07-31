9a12511969cd80bbcaa41e998ce18051be46c71c
Mario Aller <jmarioav@me.com>
2023-07-31 19:15:57 +0200

    update jul31th-4 quad/api data model change

---
+++ staff/mario-aller/fullstack/zOld/quad/quad-08/app/src/view/modals/BlockDelete.jsx
@@ -0,0 +1,28 @@
+import { deleteBlock } from '../../logic/panel-ctrl'
+import context from '../../context'
+
+export function BlockDelete({ onDeletedBlock, onExitModal, blockId }) {
+    const handleOnExitModal = () => onExitModal()
+    const handleUpdatePost = (event) => {
+        event.preventDefault()
+
+        try {
+            deleteBlock(context.tokenUser, blockId)
+                .then(() => onDeletedBlock())
+                .catch(error => alert('Error: ' + error.message))
+        } catch (error) { alert('Error: ' + error.message) }
+    }
+
+    return <>
+        {<div className="home-modal-editpost basic-modal">
+            <form className="home-modal-editpost-form basic-form" action="submit" onSubmit={handleUpdatePost}>
+                <h4>Delete block</h4>
+
+                <div className="flex-hor">
+                    <button type="submit" className="basic-button">Delete</button>
+                    <button type="button" className="basic-button" onClick={handleOnExitModal}>Cancel</button>
+                </div>
+            </form>
+        </div>}
+    </>
+}
