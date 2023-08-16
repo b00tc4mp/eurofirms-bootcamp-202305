3963ea1dd9a960ed6a67b8fe6ca90999daa7650c
Cristian Giraldez Seijo <crisdemos@gmail.com>
2023-08-16 13:14:57 +0200

    sanitize retrieve story api logic

--- staff/cristian-giraldez-seijo/fullstack/project/api/logic/retrieveStory.js
+++ staff/cristian-giraldez-seijo/fullstack/project/api/logic/retrieveStory.js
@@ -4,9 +4,18 @@ const { Story } = require('../data')
 function retrieveStory(storyId) {
     validateId(storyId)
 
-    return Story.findById(storyId, '-__v').lean()
+    return Story.findById(storyId, '-__v').populate('author', 'nickname').lean()
         .then(story => {
             if (!story) throw new Error('story not found!')
+
+            // sanitize
+            if (story.author._id) {
+                story.author.id = story.author._id.toString()
+                delete story.author._id
+            }
+
+            story.id = story._id.toString()
+            delete story._id
 
             return story
         })
