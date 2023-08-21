c7221296ede62fc0147c185a18413084c79e78b7
Mario Aller <jmarioav@me.com>
2023-08-21 17:20:27 +0200

    tailwin again

---
+++ staff/mario-aller/fullstack/zzz/quad/quad-40/com/index.js
@@ -0,0 +1,9 @@
+const sleep = require('./sleep')
+const display = require('./display')
+const validateString = require('./validateString')
+
+module.exports = {
+    sleep,
+    display,
+    validateString
+}
